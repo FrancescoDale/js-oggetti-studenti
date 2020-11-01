15ecbd3a0a13a5c848f4cf340b2205cafb6e0f60
FrancescoDale <fd060862@gmail.com>
2020-11-01 10:07:34 +0100

    creazione oggetto studente

---
+++ assets/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    // creo un oggetto che rappresenta uno studente
+
+    var student;
+
+    student = {
+        'name' = 'Tizio';
+        'surname' = 'Tizii';
+        'age' = 25;
+    }
+    
+}
+);
